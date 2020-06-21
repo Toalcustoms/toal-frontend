94bcf85c1f05518e55ac29efa9d47d89ce2436a8
Johannes Hartl <johannes.hartl@protonmail.com>
2020-06-21 17:21:06 +0200

    add home skeleton + tailwind

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-    <nav>
-      <router-link
-        class="router"
-        v-for="routes in links"
-        v-bind:key="routes.id"
-        :to="`${routes.page}`"
-        >{{ routes.text }}</router-link
+  <header class="text-gray-700 body-font">
+    <div
+      class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+    >
+      <a
+        class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
       >
-    </nav>
-  </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-10 h-10 text-white p-2 bg-teal-500 rounded-full"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
+          ></path>
+        </svg>
+        <span class="ml-3 text-xl">toalcustoms</span>
+      </a>
+      <nav
+        class="md:ml-auto flex flex-wrap items-center text-lg justify-center"
+      >
+        <a class="mr-6 hover:text-teal-500">About</a>
+        <a class="mr-6 hover:text-teal-500 line-through">Shop</a>
+        <a class="mr-6 hover:text-teal-500">Contact</a>
+      </nav>
+      <button
+        class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-lg mt-4 md:mt-0 hover:text-teal-500"
+      >
+        Log In / Sign Up
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-4 h-4 ml-1"
+          viewBox="0 0 24 24"
+        >
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </button>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
@@ -37,8 +74,4 @@ export default class Navigation extends Vue {
 }
 </script>
 
-<style>
-.router {
-  margin-right: 10px;
-}
-</style>
+<style></style>
